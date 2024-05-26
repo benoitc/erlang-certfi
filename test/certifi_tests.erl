9b4a5e0fcb2d3f947cfd9dbd4d73a550d0455ddd
Boku Kihara <boku_kihara@dwango.co.jp>
2024-05-26 12:47:02 +0900

    Introduced fixed certificates for tests

--- test/certifi_tests.erl
+++ test/certifi_tests.erl
@@ -2,6 +2,9 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%% Update this when updating priv/cacerts.pem.
+-define(NUM_OF_CERTS, 147).
+
 -ifdef('OTP_20_AND_ABOVE').
 reproducible_module_test() ->
     %% When compiled with +deterministic, only version is left out.
@@ -9,10 +12,14 @@ reproducible_module_test() ->
 -endif.
 
 cacerts_test_() ->
-    Certs = [Cert1, Cert2, Cert3 | _] = certifi:cacerts(),
-    [?_assertEqual(128, length(Certs))
-    ,?_assertMatch(<<48,130,5,192,48,130,3,168,160,3,2,1,2,2,16,30,191,89,80,184,_/binary>>, Cert1)
-    ,?_assertMatch(<<48,130,2,101,48,130,1,235,160,3,2,1,2,2,16,120,143,39,92,_/binary>>, Cert2)
-    ,?_assertMatch(<<48,130,5,239,48,130,3,215,160,3,2,1,2,2,8,13,211,227,188,_/binary>>, Cert3)
-    ,?_assertMatch(<<48,130,3,117,48,130,2,93,160,3,2,1,2,2,11,4,0,0,0,0,1,21,75,90,195,148,48,13,6,_/binary>>, lists:last(Certs))
+    Certs = certifi:cacerts(),
+    [?_assertEqual(?NUM_OF_CERTS, length(Certs))
+    ].
+
+cacerts_test_data_test_() ->
+    Certs = [Cert1, Cert2, Cert3] = certifi:cacerts_test_data(),
+    [?_assertEqual(3, length(Certs))
+    ,?_assertMatch(<<48,130,3,199,48,130,2,175,160,3,2,1,2,2,20,46,59,44,50,129,_/binary>>, Cert1)
+    ,?_assertMatch(<<48,130,3,199,48,130,2,175,160,3,2,1,2,2,20,22,71,8,124,36,_/binary>>, Cert2)
+    ,?_assertMatch(<<48,130,3,199,48,130,2,175,160,3,2,1,2,2,20,39,199,152,45,116,_/binary>>, Cert3)
     ].
